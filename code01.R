f006159c8ad1f978de5afd2852199a7a9b9cafa9
ChihiroYNU <fujiwara-chihiro-mf@ynu.jp>
2026-05-12 12:06:09 +0900

    データを取り出した。

--- code01.R
+++ code01.R
@@ -1,4 +1,34 @@
 #install.packages("tidyverse")
 library(tidyverse)
+library(readr)
 
-ab
+
+raw_data_2023 <- read_csv(
+  "SSDSE-A-2023.csv",
+  locale = locale(encoding = "CP932"),
+  skip = 2
+)
+raw_data_2024 <- read_csv(
+  "SSDSE-A-2024.csv",
+  locale = locale(encoding = "CP932"),
+  skip = 2
+)
+raw_data_2025 <- read_csv(
+  "SSDSE-A-2025.csv",
+  locale = locale(encoding = "CP932"),
+  skip = 2
+)
+
+
+data_2023 <- raw_data_2023 |> 
+ dplyr::select("地域コード":"市区町村","経常収支比率（市町村財政）"
+                :"高等学校生徒数")
+
+data_2024 <- raw_data_2024 |> 
+  dplyr::select("地域コード":"市区町村","経常収支比率（市町村財政）"
+                :"高等学校生徒数")
+
+data_2025 <- raw_data_2025 |> 
+  dplyr::select("地域コード":"市区町村","経常収支比率（市町村財政）"
+                :"高等学校生徒数")
+
