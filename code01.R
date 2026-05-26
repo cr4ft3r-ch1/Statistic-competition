bec298f11d5e33ea37f91783f11ccd8e81c90fae
ChihiroYNU <fujiwara-chihiro-mf@ynu.jp>
2026-05-26 14:27:48 +0900

    パネルデータの修正

--- code01.R
+++ code01.R
@@ -15,37 +15,44 @@ data_2018 <- read_csv(
   "SSDSE-A-2018.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2018)
+)|> dplyr::mutate(year = 2018,
+                  education_year = 2015)
 data_2019 <- read_csv(
   "SSDSE-A-2019.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2019)
+)|> dplyr::mutate(year = 2019,
+                  education_year = 2016)
 data_2020 <- read_csv(
   "SSDSE-A-2020.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2020)
+)|> dplyr::mutate(year = 2020,
+                  education_year = 2017)
 data_2021 <- read_csv(
   "SSDSE-A-2021.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2021)
+)|> dplyr::mutate(year = 2021,
+                  education_year = 2018)
 data_2023 <- read_csv(
   "SSDSE-A-2023.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2023)
+)|> dplyr::mutate(year = 2023,
+                  education_year = 2019)
 data_2024 <- read_csv(
   "SSDSE-A-2024.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2024)
+)|> dplyr::mutate(year = 2024,
+                  education_year = 2020)
 data_2025 <- read_csv(
   "SSDSE-A-2025.csv",
   locale = locale(encoding = "CP932"),
   skip = 2
-)|> dplyr::mutate(year = 2025)
+)|> dplyr::mutate(year = 2025,
+                  education_year = 2021)
 
 
 #パネルデータ化
@@ -54,7 +61,7 @@ data_2025 <- read_csv(
 f_muni <- function(x) {
   x |> 
     dplyr::select(
-      year, # 追加: 結合時に付与した年度列を残す
+      year, education_year,# 追加: 結合時に付与した年度列を残す
       "地域コード":"市区町村",
       "経常収支比率（市町村財政）":"高等学校生徒数"
     ) |> 
@@ -115,7 +122,7 @@ panel_data_muni <- bind_rows(data_2018, data_2019,data_2020, data_2021, data_202
 
 # 県で分ける
 panel_data_pre <- panel_data_muni |> 
-  dplyr::group_by(prefecture, year) |> 
+  dplyr::group_by(prefecture,education_year) |> 
   dplyr::summarise(dplyr::across(c(ordinary_balance_ratio,real_debt_service_ratio),
                                
                                ~mean(.x, na.rm = TRUE),
@@ -139,9 +146,47 @@ dplyr::across(where(is.numeric)&
               .names = "pre_{.col}"
               
 ),.groups = "drop"
-)
+) |> 
+  dplyr::mutate(
+    region = dplyr::case_when(
+      prefecture %in% c("北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県") ~ "北海道・東北地方",
+      prefecture %in% c("新潟県", "富山県", "石川県", "福井県") ~ "北陸地方",
+      prefecture %in% c("茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県") ~ "関東地方",
+      prefecture %in% c("山梨県", "長野県", "岐阜県", "静岡県", "愛知県") ~ "中部地方",
+      prefecture %in% c("三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県") ~ "近畿地方",
+      prefecture %in% c("鳥取県", "島根県", "岡山県", "広島県", "山口県") ~ "中国地方",
+      prefecture %in% c("徳島県", "香川県", "愛媛県", "高知県") ~ "四国地方",
+      prefecture %in% c("福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県") ~ "九州・沖縄地方",
+      TRUE ~ NA_character_
+    ),
+    metro_area = dplyr::case_when(
+      prefecture %in% c("東京都", "神奈川県", "埼玉県", "千葉県") ~ "首都圏",
+      prefecture %in% c("大阪府", "京都府", "兵庫県", "奈良県") ~ "近畿都市圏",
+      prefecture %in% c("愛知県", "岐阜県", "三重県") ~ "中京圏",
+      prefecture %in% c("茨城県", "栃木県", "群馬県", "滋賀県", "和歌山県", "静岡県") ~ "都市圏近郊",
+      TRUE ~ "地方圏"
+    )
+  ) |>
+  dplyr::relocate(metro_area, .after = region)|>
+  dplyr::mutate(
+    pre_education_expences_perstudents = pre_education_expense / (pre_elementary_school_students + pre_junior_high_school_students + pre_high_school_students)
+  )
 
 
+# 2. グラフの作成(折れ線グラフ)
+ggplot2::ggplot(panel_data_pre, aes(x = education_year, y = log(pre_education_expences_perstudents), color = prefecture)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  labs(title = "地域別の推移", x = "年度", y = "値")+
+  facet_wrap( ~ region )
+
+ggplot2::ggplot(panel_data_muni, aes(x = year, y = log(education_expences_perstudents), color = prefecture)) +
+  ylim(c(9,40)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  labs(title = "地域別の推移", x = "年度", y = "値")
 
 
 
@@ -151,6 +196,7 @@ dplyr::across(where(is.numeric)&
    data = data_2023,
    title = "表1：記述統計量一覧"
  )
+ 
 # 教育費の分布をプロット
 education_distribution <- ggplot2::ggplot() +
    ggplot2::geom_histogram(data = data_2023, 
